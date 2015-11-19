346bdc0bb22a81a0159f8a6c66415430ad3bea6b
Nathan Menge <mengen@turnitin.com>
2015-11-19 08:35:21 -0500

    Adding OSX support to postgres install script

--- script/postgresql_install.sh
+++ script/postgresql_install.sh
@@ -1,9 +1,20 @@
 #!/usr/bin/bash
 
-apt-get install postgresql
-apt-get install libpq-dev
-bundle install
-# Create user role in postgres
-sudo -u postgres bash -c "psql -c \"CREATE USER hashpay SUPERUSER PASSWORD 'password1';\""
-# run db setup script
-sh script/db_rebuild.sh
+if [[ -z $(which postgres) ]]; then
+  #If this is a Mac
+  if [[ $OSTYPE == darwin* ]]
+    then
+      brew install postgresql
+      bundle install
+      postgres -D /usr/local/var/postgres/
+      psql -c "CREATE USER test SUPERUSER PASSWORD 'password1';" postgres
+    else #assume ubuntu linux
+      apt-get install postgresql
+      apt-get install libpq-dev
+      bundle install
+      sudo -u postgres bash -c "psql -c \"CREATE USER hashpay SUPERUSER PASSWORD 'password1';\""
+  fi
+  # run db setup script
+  sh script/db_rebuild.sh
+fi
+
